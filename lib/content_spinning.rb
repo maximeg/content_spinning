e2e49e12e7b8a464c45a0dde4f433325cbb03647
Maxime Garcia <maxime.garcia@gmail.com>
2016-05-15 19:12:40 +0200

    Perf: Review regexp usage (-74% in time)

--- lib/content_spinning.rb
+++ lib/content_spinning.rb
@@ -12,15 +12,18 @@ module ContentSpinning
       spin_all_level(result[:parsed], result[:max_level])
     end
 
+    EMPTY_SPIN_REGEXP = /\{\|*\}/
+    ONE_CHOICE_SPIN_REGEXP = /\{([^\{\}\|]+)\}/
+
     def clean(text)
       loop do
         text_before_run = text.dup
 
         # Strip empty spin
-        text.gsub!(/\{\|*\}/, "")
+        text.gsub!(EMPTY_SPIN_REGEXP, "")
 
         # Remove spin with only one choice
-        text.gsub!(/\{([^\{\}\|]+)\}/, '\1')
+        text.gsub!(ONE_CHOICE_SPIN_REGEXP, '\1')
 
         break if text == text_before_run
       end
@@ -28,17 +31,21 @@ module ContentSpinning
       text
     end
 
+    INNER_SPIN_REGEXP = /\{([^\{\}]+)\}/
+
     def parse(text, level = 1)
       return { parsed: text, max_level: level - 1 } unless text.include?("{")
 
-      text.gsub!(/\{([^\{\}]+)\}/) do |match|
-        match.gsub!(/\{/, "__SPIN_BEGIN_#{level}__")
-        match.gsub!(/\}/, "__SPIN_END_#{level}__")
-        match.gsub!(/\|/, "__SPIN_OR_#{level}__")
+      text.gsub!(INNER_SPIN_REGEXP) do |match|
+        match.gsub!("{", "__SPIN_BEGIN_#{level}__")
+        match.gsub!("}", "__SPIN_END_#{level}__")
+        match.gsub!("|", "__SPIN_OR_#{level}__")
       end
 
       parse(text, level + 1)
     end
+
+    PARTITIONNER_REGEXP_FOR_LEVEL = Hash.new { |h, level| h[level] = /__SPIN_BEGIN_#{level}__.+?__SPIN_END_#{level}__/ }
 
     def spin_a_level(text_or_array, level)
       content_array = text_or_array.is_a?(Array) ? text_or_array : [text_or_array]
@@ -50,10 +57,11 @@ module ContentSpinning
       content_array.map! do |text|
         if text.include?(spin_begin)
           # Spin a first one
-          before, vary, after = text.partition(Regexp.new(spin_begin + ".+?" + spin_end))
-          vary.gsub!(Regexp.union(spin_begin, spin_end), "")
+          before, vary, after = text.partition(PARTITIONNER_REGEXP_FOR_LEVEL[level])
+          vary.gsub!(spin_begin, "")
+          vary.gsub!(spin_end, "")
 
-          varies = vary.split(Regexp.new(spin_or), -1)
+          varies = vary.split(spin_or, -1)
           varies.map! { |choice| before + choice + after }
 
           # Continue spinning the level if there are other same level spin or just return
